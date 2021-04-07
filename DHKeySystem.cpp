ddc15d6ffca9e4c23497f717da1a999f20d3b857
DianRichards <58058699+Majikku@users.noreply.github.com>
2021-04-07 15:02:56 +1200

    Added some the functions
    Note to_string doesn't breaks the compiler on windows. Need to test this on linux.

--- DHKeySystem.cpp
+++ DHKeySystem.cpp
@@ -1,42 +1,68 @@
+
 #include <iostream>
-// Functions:
-// createPublicKey();
-//      Base prime "p"
-//      Secret key "a,b"
-//      Public seed "g"
+#include <string>
 
-// createEncryptedKey();
-//      Received public
-//      Secret key
-//      Base prime
+// Base prime "p"
+// Secret key "k"
+// Public seed "g"
+int createPublicKey(int p, int sk, int g){
+    return ((g^sk)%p);
+};
 
-// encryptedMessage();
+// Public key "pk"
+// Secret key "sk"
+// Base prime "p"
+int createEncryptedKey(int pk, int sk, int p){
+    return ((pk^sk)%p);
+};
+
+// encryptMessage();
 //      Plain message
 //      Encrypted key
 //      Base prime
+
+
+//Plain message "msg"
+// Encrypted key "ek"
+// Base prime "p"
+std::string encryptMessage(std::string msg, int ek, int p){
+    std::string intStr;
+    for(int i = 0; i<=msg.length(); i++){
+        int intChar = int(msg[i]);
+        intStr.append("b");
+        //std::cout << intStr << "\n";
+    };
+    return intStr;
+};
 
 // decryptMessage();
 //      Encrypted message
 //      EncryptedKey
 //      Base prime
 
-// breakKey()
-//      Public seed
-//      Base prime
-//      Public key
-
 
 // !!--NEED TO IMPORVE UPON--!!
-int breakKey(int g, int p, int ga){
+// Pubic seed "g"
+// Base prime "p"
+// Public key "pk"
+int breakKey(int g, int p, int pk){
     int a = 1;
-    while (((g^a)%p) != ga){
+    while (((g^a)%p) != pk){
         a++;
     }
     return a;
 };
 
 int main() {
-    int sum = breakKey(9,7,4);
-    std::cout << sum << "\n";
+    //int sum = breakKey(9,7,4);
+    //std::cout << sum << "\n";
+
+    //std::string tst = encryptMessage("a",1,1);
+    //std::cout << tst << "\n";
+    std::string tst = "something";
+    int test = 123;
+    tst += std::to_string(123);
+    std::cout << tst << "\n";
+    
     return 0;
 };
